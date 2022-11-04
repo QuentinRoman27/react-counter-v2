917bbd6567e791417880676613a6b98748229730
Quentin <alixetquentin@quentin-roman.lan>
2022-11-04 17:53:03 +0100

    exo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = (click) => {
+    if (click === "less") {
+      setCounter(counter - 1);
+    } else if (click === "more") {
+      setCounter(counter + 1);
+    } else if (click === "reset") {
+      setCounter(0);
+    }
+  };
+
+  const [tabCounter, setTabCounter] = useState([0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          onClick={() => {
+            const newTabCounter = [...tabCounter];
+
+            newTabCounter.push(0);
+            setTabCounter(newTabCounter);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Counter
+        </button>
+      </div>
+      <div className="buttons-container">
+        <button
+          onClick={() => {
+            handleClick("less");
+          }}
+        >
+          -
+        </button>
+
+        <span>{counter}</span>
+
+        <button
+          onClick={() => {
+            handleClick("more");
+          }}
+        >
+          +
+        </button>
+      </div>
+      <button
+        onClick={() => {
+          handleClick("reset");
+        }}
+        className="reset"
+      >
+        Reset
+      </button>
+
+      {/* deuxieme pavé
+       */}
+
+      <div className={tabCounter.length > 1 ? "" : "hide"}>
+        <div className="buttons-container">
+          <button
+            onClick={() => {
+              const newTabCounter = [...tabCounter];
+
+              newTabCounter[1]--;
+              setTabCounter(newTabCounter);
+            }}
+          >
+            -
+          </button>
+
+          <span>{tabCounter[1]}</span>
+
+          <button
+            onClick={() => {
+              const newTabCounter = [...tabCounter];
+
+              newTabCounter[1]++;
+              setTabCounter(newTabCounter);
+            }}
+          >
+            +
+          </button>
+        </div>
+        <button
+          onClick={() => {
+            const newTabCounter = [...tabCounter];
+
+            newTabCounter[1] = 0;
+            setTabCounter(newTabCounter);
+          }}
+          className="reset"
+        >
+          Reset
+        </button>
+        <div className={tabCounter.length > 2 ? null : "hide"}>
+          <div className="buttons-container">
+            <button
+              onClick={() => {
+                const newTabCounter = [...tabCounter];
+
+                newTabCounter[2]--;
+                setTabCounter(newTabCounter);
+              }}
+            >
+              -
+            </button>
+
+            <span>{tabCounter[2]}</span>
+
+            <button
+              onClick={() => {
+                const newTabCounter = [...tabCounter];
+
+                newTabCounter[2]++;
+                setTabCounter(newTabCounter);
+              }}
+            >
+              +
+            </button>
+          </div>
+          <button
+            onClick={() => {
+              const newTabCounter = [...tabCounter];
+
+              newTabCounter[2] = 0;
+              setTabCounter(newTabCounter);
+            }}
+            className="reset"
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
